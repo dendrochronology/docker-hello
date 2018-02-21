7188425023401c92463f0f426754290e88d10e26
Aaron Fuleki <aaron@fuleki.org>
2018-02-21 12:25:25 -0500

    Add request logging

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const PORT = process.env.PORT || 80;
 
 // App
 const app = express();
+
+var requestLogger = (req, res, next) => {
+  const date = new Date();
+  const method = req.method;
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(`${date.toISOString()} ${method} ${ip} ${req.originalUrl}`);
+  next()
+}
+
+app.use(requestLogger)
+
 app.get('/', (req, res) => {
   res.send('<html><body><h1>Hello World</h1></body></html>\n');
 });
